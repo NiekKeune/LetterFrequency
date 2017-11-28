a3e9b1c130ccdc1b19efd34cca2f11b742deb2ef
Niek Keunegit config -- global user.email "Niek.Keune@hva.nl"git clone git@gitlab.dmci.hva.nl:info/git4bit.gitgit config --global user.name "Niek Keune <niekkeune@gmail.com>
2017-11-28 15:21:41 +0100

    Changed maps
    See title.

---
+++ Testing3/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace LetterFrequency
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CountLetters = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CountLetters
+            // 
+            this.CountLetters.Location = new System.Drawing.Point(12, 41);
+            this.CountLetters.Name = "CountLetters";
+            this.CountLetters.Size = new System.Drawing.Size(50, 43);
+            this.CountLetters.TabIndex = 0;
+            this.CountLetters.Text = "Count";
+            this.CountLetters.UseVisualStyleBackColor = true;
+            this.CountLetters.Click += new System.EventHandler(this.CountLetters_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(92, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(70, 43);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.CountLetters);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CountLetters;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Testing3/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LetterFrequency
+{
+    public partial class Form1 : Form
+    {
+        string placeholder = "A sïmple русский 한글또는조선글은 test";  //test string to work with, tests different letters than the standard alphabet
+        int count = 0;                                                //variable which will count how many letters there are
+
+        public void Counting()
+        {
+            foreach (char letter in placeholder)                      //goes through the string
+            {
+                if (char.IsLetter(letter))                            //checks if its a letter and not a character
+                {
+                    count++;                                          //adds a number to the count variable
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            List<String> letters = new List<String>();                      //test list to visualize how I want it to look
+            letters.Add("a");
+            letters.Add("B");
+            letters.Add("µ");
+            letters.Add("ð");
+
+            textBox1.Text = String.Join(Environment.NewLine, letters);
+        }
+
+        public string displayMembers(List<String> letters)
+        {
+            foreach (String s in letters)
+            {
+                return s.ToString();
+            }
+            return null;
+        }
+
+        private void CountLetters_Click(object sender, EventArgs e)
+        {
+            Counting();                                             //goes to the counting method when the button is pressed
+            string result = count.ToString();                       //converts int count to string result variable
+            textBox1.Text = result;                                 //textbox displays the result
+            count = 0;                                              //count resetted to 0 so the counting method doesnt add up if the button is pressed again
+        }
+
+    }
+}
